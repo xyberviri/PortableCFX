aedebec8f3125196af6febc3694e9554fc5075b5
James Velasquez <xyberviri@gmail.com>
2023-08-12 00:22:15 -0400

    Update 7.Clear_Cache_SERVER.cmd

--- 7.Clear_Cache_SERVER.cmd
+++ 7.Clear_Cache_SERVER.cmd
@@ -1,18 +1,15 @@
 @ECHO OFF
-FOR /F "tokens=*" %%g IN ('dir server_files\txData\QBCore* /b') do (SET directoryName=%%g)
 SETLOCAL EnableExtensions
 set EXE=FXServer.exe
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF NOT %%x == %EXE% (
 ECHO clearing server cache
-rmdir server_files\txData\%directoryName%\cache\files /S /Q
-mkdir server_files\txData\%directoryName%\cache\files
+FOR /F "tokens=*" %%g IN ('dir server_files\txData\*.base /b') do (
+ECHO %%g
+rmdir "server_files\txData\%%g\cache\files" /S /Q
+mkdir "server_files\txData\%%g\cache\files"
+)
 pause
 exit
 )
 ECHO DON'T RUN THIS WHILE THE SERVER IS RUNNING!!!
-PAUSE
-
-
-
-
-
+pause
