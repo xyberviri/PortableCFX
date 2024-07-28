409aa38c1ac517dcec8510818084ee153d712394
James Velasquez <xyberviri@gmail.com>
2024-07-28 18:11:34 -0400

    updated shutdown command

--- 5.Shutdown_Database.cmd
+++ 5.Shutdown_Database.cmd
@@ -3,7 +3,7 @@ SETLOCAL EnableExtensions
 set EXE=FXServer.exe
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF NOT %%x == %EXE% (
 ECHO Shutting down database
-"mariadb-10.4.30-winx64\bin\mysqladmin.exe" -u root shutdown
+"mariadb-11.4.2-winx64\bin\mysqladmin.exe" -u root shutdown
 GOTO END
 )
 ECHO DON'T RUN THIS WHILE THE FIVEM SERVER IS RUNNING!!!
